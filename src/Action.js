524543cc380d71ab4b1ef5d03c9da34e6bcfe57d
Abrar051 <shahriar1410051@gmail.com>
2021-08-16 19:05:18 +0600

    >>>

--- src/Action.js
+++ src/Action.js
@@ -1,3 +1,24 @@
 function myFunction() {
-    document.getElementById("demo").innerHTML = "Hello";
+    alert("Hello");
+    //document.getElementById("demo").innerHTML = "Hello";
+}
+
+function getUserName () {
+    let username = prompt("Enter UserName : ", "Enter name here.");
+    if (username) {
+        alert("It is good to meet you, " + username + ".");
+    }
+}
+
+function getUserNameStyle2 ()
+{
+    let username = prompt("Enter UserName : ", "Enter name here.");
+    if (username) {
+        console.log("It is good to meet you, " + username + ".");
+    }
+}
+
+function replace ()
+{
+
 }
